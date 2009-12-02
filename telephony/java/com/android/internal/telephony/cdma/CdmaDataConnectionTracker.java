48f7409f27ffaf459aac5708e8618de5ab95cf6b
Shweta Kumari <skumari@qualcomm.com>
2009-12-02 15:55:57 +0530

    Telephony : Added option to disable socket data call during boot up.
    Removed DUN_ENABLE from Settings as it is no longer used.Added
    SOCKET_DATA_CALL_ENABLE that will be set in enableDataConnectivity()
    and reset in disableDataConnectivity().This value will be read in
    trySetUpData() in DataConnectionTracker files and no PDP context
    is established if the SOCKET_DATA_CALL_ENABLE is false.
    
    CRs-Fixed : 193354

--- telephony/java/com/android/internal/telephony/cdma/CdmaDataConnectionTracker.java
+++ telephony/java/com/android/internal/telephony/cdma/CdmaDataConnectionTracker.java
@@ -185,12 +185,9 @@ public final class CdmaDataConnectionTracker extends DataConnectionTracker {
 
         createAllDataConnectionList();
 
-        // This preference tells us 1) initial condition for "dataEnabled",
-        // and 2) whether the RIL will setup the baseband to auto-PS attach.
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(phone.getContext());
-
-        dataEnabled[APN_DEFAULT_ID] =
-                !sp.getBoolean(CDMAPhone.DATA_DISABLED_ON_BOOT_KEY, false);
+        // The data call state is read during reboot.
+        dataEnabled[APN_DEFAULT_ID] = getSocketDataCallEnabled();
+        Log.d(LOG_TAG, "data enabled =" + dataEnabled[APN_DEFAULT_ID]);
         if (dataEnabled[APN_DEFAULT_ID]) {
             enabledCount++;
         }
@@ -316,6 +313,11 @@ public final class CdmaDataConnectionTracker extends DataConnectionTracker {
 
             Log.i(LOG_TAG, "(fix?) We're on the simulator; assuming data is connected");
             return true;
+        }
+
+        if (!getSocketDataCallEnabled()) {
+            Log.i(LOG_TAG, "Socket data call is disabled");
+            return false;
         }
 
         int psState = mCdmaPhone.mSST.getCurrentCdmaDataConnectionState();
