1f3dc2b92c0f7b836f3b4342e729c91ad82b0505
Shailender Karmuchi <c_skarmu@qualcomm.com>
2009-12-11 18:13:26 -0800

    Replace wifi interface name from tiwlan0 to wlan0.
    WifiStateTracker hard code the interface name to tiwlan0. Replacing it
    with wlan0.

--- wifi/java/android/net/wifi/WifiStateTracker.java
+++ wifi/java/android/net/wifi/WifiStateTracker.java
@@ -343,7 +343,7 @@ public class WifiStateTracker extends NetworkStateTracker {
 
         mSettingsObserver = new SettingsObserver(new Handler());
 
-        mInterfaceName = SystemProperties.get("wifi.interface", "tiwlan0");
+        mInterfaceName = SystemProperties.get("wifi.interface", "wlan0");
         sDnsPropNames = new String[] {
             "dhcp." + mInterfaceName + ".dns1",
             "dhcp." + mInterfaceName + ".dns2"
