aeb9f7ddcaa8c57267c2c8b9fbdd6fd363d5f450
Jaime Lopez <c_jaimel@quicinc.com>
2010-02-25 13:45:52 -0800

    frameworks/base/telephony: Ignore events on a destroyed phone
    	It is possible to receive responses from RIL after phone has released
    	it's references. Ignore events in such cases, since those events no
    	longer make sense, and some phone members have been de-initialized.
    
    	CRs-Fixed: 228005

--- telephony/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ telephony/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -1249,6 +1249,11 @@ public class GSMPhone extends PhoneBase {
         AsyncResult ar;
         Message onComplete;
 
+        if (!mIsTheCurrentActivePhone) {
+            Log.e(LOG_TAG, "Received message " + msg +
+                    "[" + msg.what + "] while being destroyed. Ignoring.");
+            return;
+        }
         switch (msg.what) {
             case EVENT_RADIO_AVAILABLE: {
                 mCM.getBasebandVersion(
