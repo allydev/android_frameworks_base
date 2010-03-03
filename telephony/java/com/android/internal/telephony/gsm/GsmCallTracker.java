a9841fd4c48228e980d44d04d870a0490bbc9643
Jaime Lopez <c_jaimel@quicinc.com>
2010-03-02 16:57:59 -0800

    frameworks/base: Notify of dismissed call on radio technology change
    	Make CallTrackers notify UI when ending calls in dispose, since
    	it has already unregistered, and won't receive the CALL_STATE_CHANGED
    
    	CRs-Fixed: 228731
    
    Change-Id: I579e69a4bbfb3adee2342d731abbdc3cdd8604a0

--- telephony/java/com/android/internal/telephony/gsm/GsmCallTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmCallTracker.java
@@ -103,14 +103,22 @@ public final class GsmCallTracker extends CallTracker {
 
         for(GsmConnection c : connections) {
             try {
-                if(c != null) hangup(c);
+                if(c != null) {
+                    hangup(c);
+                    // Since by now we are unregistered, we won't notify
+                    // PhoneApp that the call is gone. Do that here
+                    c.onDisconnect(Connection.DisconnectCause.LOST_SIGNAL);
+                }
             } catch (CallStateException ex) {
                 Log.e(LOG_TAG, "unexpected error on hangup during dispose");
             }
         }
 
         try {
-            if(pendingMO != null) hangup(pendingMO);
+            if(pendingMO != null) {
+                hangup(pendingMO);
+                pendingMO.onDisconnect(Connection.DisconnectCause.LOST_SIGNAL);
+            }
         } catch (CallStateException ex) {
             Log.e(LOG_TAG, "unexpected error on hangup during dispose");
         }
