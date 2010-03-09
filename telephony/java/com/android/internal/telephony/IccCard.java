b3e1c2b39cdbb9e10120319d0fae0f4e00a5d140
Shruthi N.K <snk@qualcomm.com>
2010-03-09 15:36:14 +0530

    telephony: Fix for phone app crash due to invalid gsm subscription app index.
    Boundary check added for gsm/cdma subscription app index while trying to retrive
    the ICC Card App.
    
    Change-Id: I1442db330b88b6a8647bdfb01811b8b59b00d2ad
    CRs-Fixed: 229344

--- telephony/java/com/android/internal/telephony/IccCard.java
+++ telephony/java/com/android/internal/telephony/IccCard.java
@@ -662,6 +662,12 @@ public abstract class IccCard {
                             CommandsInterface.SERVICE_CLASS_DATA +
                             CommandsInterface.SERVICE_CLASS_FAX;
 
+	    if (!mPhone.mIsTheCurrentActivePhone) {
+		Log.e(mLogTag, "Received message " + msg +
+		    "[" + msg.what + "] while being destroyed. Ignoring.");
+		return;
+	    }
+
             switch (msg.what) {
                 case EVENT_RADIO_OFF_OR_NOT_AVAILABLE:
                     mState = null;
@@ -827,8 +833,13 @@ public abstract class IccCard {
             else {
                 index = mIccCardStatus.getGsmUmtsSubscriptionAppIndex();
             }
-
-            IccCardApplication app = mIccCardStatus.getApplication(index);
+	    IccCardApplication app;
+	    if ((index < mIccCardStatus.CARD_MAX_APPS) && (index >= 0)) {
+		app = mIccCardStatus.getApplication(index);
+	    } else {
+		Log.e(mLogTag, "[IccCard] Invalid Subscription Application index:" + index);
+		return IccCard.State.ABSENT;
+	    }
 
             if (app == null) {
                 Log.e(mLogTag, "[IccCard] Subscription Application in not present");
