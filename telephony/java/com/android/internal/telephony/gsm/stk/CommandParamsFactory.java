789c7fe67e4f9aaee160b018eea9fecaeb4528d9
Pawan Kumar Nimmagadda <pawan@qualcomm.com>
2010-03-16 00:10:35 +0530

    telephony:Framework support for Provide Local Information(PLI) Language setting.
    Changes done in STK framework to handle PLI proactive command for Language
    Setting. Framework sends terminal reponse with the locale language setting
    value read from "persist.sys.language" system property.
    
    Change-Id: Ia7e84ab0cef19b0c5bfa89c72e6703c09737215d
    CRs-Fixed: 229786

--- telephony/java/com/android/internal/telephony/gsm/stk/CommandParamsFactory.java
+++ telephony/java/com/android/internal/telephony/gsm/stk/CommandParamsFactory.java
@@ -55,6 +55,9 @@ class CommandParamsFactory extends Handler {
     static final int REFRESH_NAA_INIT                       = 0x03;
     static final int REFRESH_UICC_RESET                     = 0x04;
 
+   // Command Qualifier values for PLI command
+   static final int LANGUAGE_SETTING			    = 0x04;
+
     static synchronized CommandParamsFactory getInstance(RilMessageDecoder caller,
             IccFileHandler fh) {
         if (sInstance != null) {
@@ -162,6 +165,9 @@ class CommandParamsFactory extends Handler {
                     break;
                 case SET_UP_EVENT_LIST:
                     cmdPending = processSetUpEventList(cmdDet, ctlvs);
+                    break;
+                case PROVIDE_LOCAL_INFORMATION:
+                    cmdPending = processProvideLocalInfo(cmdDet, ctlvs);
                     break;
                 default:
                     // unsupported proactive commands
@@ -916,6 +922,22 @@ class CommandParamsFactory extends Handler {
             mIconLoader.loadIcons(recordNumbers, this
                     .obtainMessage(MSG_ID_LOAD_ICON_DONE));
             return true;
+        }
+        return false;
+    }
+
+    private boolean processProvideLocalInfo(CommandDetails cmdDet, List<ComprehensionTlv> ctlvs)
+            throws ResultException {
+        StkLog.d(this, "process ProvideLocalInfo");
+        switch (cmdDet.commandQualifier) {
+            case LANGUAGE_SETTING:
+                StkLog.d(this, "PLI [LANGUAGE_SETTING]");
+                mCmdParams = new CommandParams(cmdDet);
+                break;
+            default:
+                StkLog.d(this, "PLI[" + cmdDet.commandQualifier + "] Command Not Supported");
+                mCmdParams = new CommandParams(cmdDet);
+                throw new ResultException(ResultCode.BEYOND_TERMINAL_CAPABILITY);
         }
         return false;
     }
