Jackeagle/caf_frameworks_base

platform/frameworks/base: Call stopRecording first then start beepSignal
- Prioritize stopRecording call to avoid recording stop beep from being
recorded onto file.

CRs-Fixed: 234429

Change-Id: I6b0f4faaafc74e452f4fe521f01250921ebca38e

camera/libcameraservice/CameraService.cpp
@@ -790,14 +790,15 @@ void CameraService::Client::stopRecording()
             return;
         }
 
+        mHardware->disableMsgType(CAMERA_MSG_VIDEO_FRAME);
+        mHardware->stopRecording();
+        LOGD("stopRecording(), hardware stopped OK");
+
         if (mMediaPlayerBeep.get() != NULL) {
             mMediaPlayerBeep->seekTo(0);
             mMediaPlayerBeep->start();
         }
 
-        mHardware->disableMsgType(CAMERA_MSG_VIDEO_FRAME);
-        mHardware->stopRecording();
-        LOGD("stopRecording(), hardware stopped OK");
     }
 
     // hold preview buffer lock

allydev/android_frameworks_base

Change-Id: Ia8c15b18699406ee2d32635fc83adecd290d0be1

camera/libcameraservice/CameraService.cpp
@@ -775,14 +775,15 @@ void CameraService::Client::stopRecording()
             return;
         }
 
+        mHardware->disableMsgType(CAMERA_MSG_VIDEO_FRAME);
+        mHardware->stopRecording();
+        LOGV("stopRecording(), hardware stopped OK");
+
         if (mMediaPlayerBeep.get() != NULL) {
             mMediaPlayerBeep->seekTo(0);
             mMediaPlayerBeep->start();
         }
 
-        mHardware->disableMsgType(CAMERA_MSG_VIDEO_FRAME);
-        mHardware->stopRecording();
-        LOGV("stopRecording(), hardware stopped OK");
     }
 
     // hold preview buffer lock