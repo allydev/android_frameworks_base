0858395c9ddf7e8fc46c578e28b96dbef90fe405
Naomi Luis <nluis@codeaurora.org>
2010-07-19 19:27:57 -0700

    Add the interlace pixel format to the list of supported formats
    Change-Id: Ib8c6bb04396ab2704452a78fc71da3e7ba32448a

--- libs/ui/PixelFormat.cpp
+++ libs/ui/PixelFormat.cpp
@@ -72,6 +72,7 @@ status_t getPixelFormatInfo(PixelFormat format, PixelFormatInfo* info)
     case HAL_PIXEL_FORMAT_YCbCr_420_P:
     case HAL_PIXEL_FORMAT_YCbCr_420_I:
     case HAL_PIXEL_FORMAT_CbYCrY_420_I:
+    case HAL_PIXEL_FORMAT_YCrCb_420_SP_INTERLACE:
         info->bitsPerPixel = 12;
      done:
         info->format = format;
