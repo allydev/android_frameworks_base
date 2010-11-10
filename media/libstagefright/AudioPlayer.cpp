387096f21637b4a84856a44c7cc7379c495e188c
Avss, Srikanth <savss@codeaurora.org>
2010-11-10 18:11:18 +0530

    libstagefright: Flush audio sink when the playback is paused
    When playback is paused and a reposition is done, the playback begins
    with few residual frames from the paused position and then the frames
    from the new position. This is because the frames which were decoded
    but not rendered when playback was paused are not flushed. Flushing
    those frames by calling audio sink stop.
    
    Change-Id: I6e3aabcebd183f6cfc098402cdba1fda015a0b4d
    CRs-Fixed: 263535

--- media/libstagefright/AudioPlayer.cpp
+++ media/libstagefright/AudioPlayer.cpp
@@ -165,7 +165,7 @@ void AudioPlayer::pause() {
     CHECK(mStarted);
 
     if (mAudioSink.get() != NULL) {
-        mAudioSink->pause();
+        mAudioSink->stop();
     } else {
         mAudioTrack->stop();
     }
