034d3d9b3b534cf4748b4d7cbdf891839e038503
getitnowmarketing <getitnowmarketing@gmail.com>
2011-01-16 00:47:59 -0500

    add build flag for LG slide kb fix
    Change-Id: I7777bb27d45359ef4a96604af15a3adaff3e792a

--- libs/ui/EventHub.cpp
+++ libs/ui/EventHub.cpp
@@ -761,8 +761,10 @@ int EventHub::open_device(const char *deviceName)
             //LOGI("Device 0x%x sw %d: has=%d", device->id, i, test_bit(i, sw_bitmask));
             if (test_bit(i, sw_bitmask)) {
                 if (mSwitches[i] == 0) {
-                    if(!device->classes)
-			device->classes = CLASS_KEYBOARD;
+#ifdef LG_SLIDE_KB_HACK                  
+		if(!device->classes)
+		device->classes = CLASS_KEYBOARD;
+#endif
 		    mSwitches[i] = device->id;
                 }
             }
